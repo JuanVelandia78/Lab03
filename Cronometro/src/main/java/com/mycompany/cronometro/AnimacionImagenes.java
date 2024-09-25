65ec61c9fa70f1b7109e71f7392d8813f4ed0590
JuanVelandia78 <est.juand.velandia@unimilitar.edu.co>
2024-09-24 20:50:14 -0500

    Version5

--- Cronometro/src/main/java/com/mycompany/cronometro/AnimacionImagenes.java
+++ Cronometro/src/main/java/com/mycompany/cronometro/AnimacionImagenes.java
@@ -3,10 +3,14 @@ package com.mycompany.cronometro;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class AnimacionImagenes extends JFrame {
     private JLabel imagenLabel;
     private JSlider velocidadSlider;
+    private Timer timer;
+    private ImageIcon[] imagenes;
+    private int indiceActual = 0;
 
     public AnimacionImagenes() {
         setTitle("Animación de Imágenes");
@@ -14,11 +18,36 @@ public class AnimacionImagenes extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        imagenLabel = new JLabel(new ImageIcon("imagen1.png"));
+        imagenes = new ImageIcon[5];
+        for (int i = 0; i < 5; i++) {
+            imagenes[i] = new ImageIcon("imagen" + (i + 1) + ".png");
+        }
+
+        imagenLabel = new JLabel(imagenes[0]);
         add(imagenLabel, BorderLayout.CENTER);
 
         velocidadSlider = new JSlider(JSlider.HORIZONTAL, 100, 2000, 1000);
+        velocidadSlider.addChangeListener(e -> actualizarVelocidad());
         add(velocidadSlider, BorderLayout.SOUTH);
+
+        iniciarAnimacion();
+    }
+
+    private void iniciarAnimacion() {
+        timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                indiceActual = (indiceActual + 1) % imagenes.length;
+                imagenLabel.setIcon(imagenes[indiceActual]);
+            }
+        });
+        timer.start();
+    }
+
+    private void actualizarVelocidad() {
+        if (timer != null) {
+            timer.setDelay(velocidadSlider.getValue());
+        }
     }
 
     public static void main(String[] args) {
